62068e043b11172f40ca32e1f7dcc351b9472fd7
Joash <colomerit2@gmail.com>
2022-02-11 16:04:49 +0800

    sql

--- assestt.sql
+++ assestt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 11, 2022 at 08:52 AM
+-- Generation Time: Feb 11, 2022 at 09:04 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `assestt`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `username`, `pass`) VALUES
+(1, 'admin', 'admin1122');
 
 -- --------------------------------------------------------
 
@@ -58,9 +77,41 @@ INSERT INTO `stocks` (`id`, `stockName`, `stockValue`, `stockQuanty`, `status`) 
 (17, 'bulalo', 500000, 1, 0),
 (18, 'Chicken Adobo', 300, 2, 0);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `username`, `pass`) VALUES
+(1, 'test1', 'test', 'test@test.com', 'test', 'test'),
+(2, 'april', 'dell', 'april@gmail.com', 'april', '1234'),
+(3, 'ching', 'chong', 'chingchong@gmail.com', 'may', 'may123'),
+(4, 'lebron banga', 'james', 'joashcolomer@gmail.com', 'james', '1122'),
+(5, 'stephenie', 'james', 'curryjame@gamil.com', 'stephen', '123');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `stocks`
@@ -69,14 +120,32 @@ ALTER TABLE `stocks`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `stocks`
 --
 ALTER TABLE `stocks`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
